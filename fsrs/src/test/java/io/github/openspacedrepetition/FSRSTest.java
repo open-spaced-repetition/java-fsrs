a04df8427a84634bd9bc6f1a40e509d5791fb4a1
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-07-01 15:23:30 -0700

    format unit tests

--- fsrs/src/test/java/io/github/openspacedrepetition/FSRSTest.java
+++ fsrs/src/test/java/io/github/openspacedrepetition/FSRSTest.java
@@ -1,14 +1,17 @@
+/* (C)2025 */
 package io.github.openspacedrepetition;
 
-import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
 
 public class FSRSTest {
 
     @Test
-    public void testReviewDefaultArg()
-    {
-        // TODO: add test similar to https://github.com/open-spaced-repetition/py-fsrs/blob/e64e3b4f710ada3486bca0e49b54ba33a3137654/tests/test_basic.py#L106
-    }
+    public void testReviewDefaultArg() {
+        // TODO: add test similar to
+        // https://github.com/open-spaced-repetition/py-fsrs/blob/e64e3b4f710ada3486bca0e49b54ba33a3137654/tests/test_basic.py#L106
 
+        System.out.println("hello world");
+    }
 }
