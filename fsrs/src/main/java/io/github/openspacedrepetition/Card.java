6d357ed210038720704cfae051a6611048c75d41
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-07-01 15:37:54 -0700

    start building Card class

--- fsrs/src/main/java/io/github/openspacedrepetition/Card.java
+++ fsrs/src/main/java/io/github/openspacedrepetition/Card.java
@@ -1,5 +1,69 @@
+/* (C)2025 */
 package io.github.openspacedrepetition;
 
+import java.time.Instant;
+
+enum State {
+    LEARNING(1),
+    REVIEW(2),
+    RELEARNING(3);
+
+    private final int value;
+
+    State(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
 public class Card {
-    
+
+    public int cardId;
+    public State state;
+    public Integer step;
+    public Double stability;
+    public Double difficulty;
+    public Instant due;
+    public Instant lastReview;
+
+    public Card(
+            Integer cardId,
+            State state,
+            Integer step,
+            Double stability,
+            Double difficulty,
+            Instant due,
+            Instant lastReview) {
+
+        if (cardId == null) {
+            cardId = (int) Instant.now().toEpochMilli();
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // restore interrupted status
+            }
+        }
+
+        this.cardId = cardId;
+
+        this.state = state;
+
+        if (this.state == State.LEARNING && step == null) {
+            step = 0;
+        }
+        this.step = step;
+
+        this.stability = stability;
+        this.difficulty = difficulty;
+
+        if (due == null) {
+            due = Instant.now();
+        }
+        this.due = due;
+
+        this.lastReview = lastReview;
+    }
 }
