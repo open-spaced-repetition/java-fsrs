8db5fc84068e5c4c05186de74c61d3e57c440f17
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-07-01 14:01:39 -0700

    add outline of first unit test

--- fsrs/src/test/java/io/github/openspacedrepetition/FSRSTest.java
+++ fsrs/src/test/java/io/github/openspacedrepetition/FSRSTest.java
@@ -1,5 +1,14 @@
 package io.github.openspacedrepetition;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FSRSTest {
-    
+
+    @Test
+    public void testReviewDefaultArg()
+    {
+        // TODO: add test similar to https://github.com/open-spaced-repetition/py-fsrs/blob/e64e3b4f710ada3486bca0e49b54ba33a3137654/tests/test_basic.py#L106
+    }
+
 }
