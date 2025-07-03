739eedfad2b7fc36a11f4a664b3405a4280ee5f1
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-07-02 20:52:25 -0700

    add basic Scheduler constructor

--- fsrs/src/main/java/io/github/openspacedrepetition/Scheduler.java
+++ fsrs/src/main/java/io/github/openspacedrepetition/Scheduler.java
@@ -1,5 +1,44 @@
+/* (C)2025 */
 package io.github.openspacedrepetition;
 
+import java.time.Duration;
+
 public class Scheduler {
-    
+
+    private static final double[] DEFAULT_PARAMETERS = {
+        0.2172, 1.1771, 3.2602, 16.1507, 7.0114, 0.57, 2.0966, 0.0069, 1.5261, 0.112, 1.0178, 1.849,
+        0.1133, 0.3127, 2.2934, 0.2191, 3.0004, 0.7536, 0.3332, 0.1437, 0.2
+    };
+    private final double DECAY;
+    private final double FACTOR;
+
+    private double[] parameters;
+    private double desiredRetention;
+    private Duration[] learningSteps;
+    private Duration[] relearningSteps;
+    private int maximumInterval;
+    private boolean enableFuzzing;
+
+    public Scheduler(
+            double[] parameters,
+            double desiredRetention,
+            Duration[] learningSteps,
+            Duration[] relearningSteps,
+            int maximumInterval,
+            boolean enableFuzzing) {
+
+        if (parameters == null) {
+            parameters = DEFAULT_PARAMETERS;
+        }
+        this.parameters = parameters;
+
+        this.desiredRetention = desiredRetention;
+        this.learningSteps = learningSteps;
+        this.relearningSteps = relearningSteps;
+        this.maximumInterval = maximumInterval;
+        this.enableFuzzing = enableFuzzing;
+
+        this.DECAY = -this.parameters[20];
+        this.FACTOR = Math.pow(0.9, 1.0 / this.DECAY) - 1;
+    }
 }
