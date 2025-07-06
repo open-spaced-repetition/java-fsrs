33d86c6b50bc93bfb00c978c6ab9ccb8850c08b6
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-07-05 18:49:49 -0700

    add testReviewCard unit test

--- fsrs/src/test/java/io/github/openspacedrepetition/FSRSTest.java
+++ fsrs/src/test/java/io/github/openspacedrepetition/FSRSTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.*;
 
 public class FSRSTest {
@@ -65,5 +68,44 @@ public class FSRSTest {
         assertTrue(
                 Duration.between(card.getLastReview(), card.getDue()).toDays()
                         <= scheduler.getMaximumInterval());
+    }
+
+    @Test
+    public void testReviewCard() {
+
+        Scheduler scheduler = new Scheduler.Builder().setEnableFuzzing(false).build();
+
+        Rating[] ratings = {
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.AGAIN,
+            Rating.AGAIN,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+            Rating.GOOD,
+        };
+
+        Card card = new Card();
+        Instant reviewDatetime = Instant.parse("2022-11-29T12:30:00Z");
+
+        List<Integer> ivlHistory = new ArrayList<>();
+        for (Rating rating : ratings) {
+
+            CardAndReviewLog result = scheduler.reviewCard(card, rating, reviewDatetime);
+            card = result.card();
+
+            int ivl = (int) ChronoUnit.DAYS.between(card.getLastReview(), card.getDue());
+            ivlHistory.add(ivl);
+
+            reviewDatetime = card.getDue();
+        }
+
+        assertEquals(List.of(0, 4, 14, 45, 135, 372, 0, 0, 2, 5, 10, 20, 40), ivlHistory);
     }
 }
