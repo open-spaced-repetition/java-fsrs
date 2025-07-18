c8bf0287f19ea6d67211180588e844723b396be1
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-07-17 21:30:43 -0700

    add copy constructor to ReviewLog class

--- src/main/java/io/github/openspacedrepetition/ReviewLog.java
+++ src/main/java/io/github/openspacedrepetition/ReviewLog.java
@@ -4,4 +4,15 @@ package io.github.openspacedrepetition;
 import java.time.Instant;
 
 public record ReviewLog(
-        int cardId, Rating rating, Instant reviewDatetime, Integer reviewDuration) {}
+        int cardId, Rating rating, Instant reviewDatetime, Integer reviewDuration) {
+
+    public ReviewLog(ReviewLog otherReviewLog) {
+        this(
+            otherReviewLog.cardId,
+            otherReviewLog.rating,
+            otherReviewLog.reviewDatetime,
+            otherReviewLog.reviewDuration
+        );
+    }
+
+}
